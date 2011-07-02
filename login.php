ed628dd4489cbf3222d3a036bcd8afbd30eb13a9
gigabeat <luismoreiraaa@gmail.com>
2011-07-02 14:57:45 -0700

    troubleshooting

--- login.php
+++ login.php
@@ -4,15 +4,16 @@ include 'star.php';
 $nome = $_POST['nome'];
 $pass = $_POST['pass'];
 
+$hashpass = sha1($pass);
+echo $hashpass;
 
-$sql="SELECT * FROM $tbl_name WHERE username = '$nome' and password='$pass'";
+$sql="SELECT * FROM $tbl_name WHERE username = '$nome' and password='$hashpass'";
 $result=mysql_query($sql);
 
 $count = mysql_num_rows($result);
 
 if($count ==1 ){
 	session_register("nome");
-	if($password == sha1($pass))
 	header("location:teste.php");	
 }
 else {
