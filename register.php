aae79675429598bac6073e07f8a1d0c3d0e1f8cf
Rubem Mota <Rubem@.(none)>
2011-07-02 18:03:22 +0200

    fix

--- register.php
+++ register.php
@@ -8,12 +8,12 @@ $tbl_name="members";
 $con = mysql_connect("$host","$username","$password") or die("Connection error");
 mysql_select_db("$db_name") or die("error selecting database");
 
-$member = $_REQUEST['member'];
+$name = $_REQUEST['name'];
 $pass = $_REQUEST['pass'];
 $email = $_REQUEST['email'];
 
 
-$sql="INSERT INTO $tbl_name(`username`,`password`,`email`) VALUES('$member','$pass', '$email')";
+$sql="INSERT INTO $tbl_name(`username`,`password`,`email`) VALUES('$name','$pass', '$email')";
 if(!mysql_query($sql,$con))
 	{
 		die(mysql_error());
