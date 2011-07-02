562c87b7595caa39abda77058c8f6e94724b35ee
gigabeat <luismoreiraaa@gmail.com>
2011-07-02 14:50:30 -0700

    espero não estar a destruir nada :o

--- login.php
+++ login.php
@@ -1,18 +1,17 @@
 <?
 include 'star.php';
 
+
 $nome = $_POST['nome'];
 $pass = $_POST['pass'];
 
-
-$sql="SELECT * FROM $tbl_name WHERE username = '$nome' and password='$pass'";
+$sql="SELECT * FROM $tbl_name WHERE username = '$nome' and password='sha1($pass)'";
 $result=mysql_query($sql);
 
 $count = mysql_num_rows($result);
 
-if($count ==1 ){
+if($count == 1 ){
 	session_register("nome");
-	if($password == sha1($pass))
 	header("location:teste.php");	
 }
 else {
